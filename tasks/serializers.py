880da0940c4b41d81d4f02cfd28fb4e14a694671
MGL1994 <michaelgl1994@gmail.com>
2020-09-20 19:13:15 +0100

    Added custom validation to Task Serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -6,3 +6,9 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('id', 'task', 'description', 'due_date')
+
+    def validate(self, data):
+        if data['task'] == data['description']:
+            raise serializers.ValidationError({'description': 'More detail required'})
+
+        return data
